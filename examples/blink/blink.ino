8a7e7facf5a3f694f840a65cd718874fbf8028eb
YuenNicdao <60959936+YuenNicdao@users.noreply.github.com>
2024-11-17 16:42:17 +0800

    fixed arduino-lint warnings

---
+++ examples/blink/blink.ino
@@ -0,0 +1,30 @@
+/*
+  Empowerment Technology
+  iParol v2 (PHS)
+  s.2023
+*/
+
+//******************************************
+// USE write() in replace to digitalWrite()
+//
+// This will work with 15x15 boards
+//******************************************
+
+#include "iParol.h"
+
+void setup() {
+    iParol_Init();
+}
+
+void loop() {
+    //you dont need to write anything here
+    for(byte i = 1; i<=225; i++){
+        write(i, HIGH);
+    }
+    delay(100);
+    
+    for(byte i = 1; i<=225; i++){
+        write(i, LOW);
+    }
+    delay(100);
+}
